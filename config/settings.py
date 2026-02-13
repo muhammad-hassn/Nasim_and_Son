478461a5c8c7343ee94f5b9e11f8f78d21bded4c
Muhammad Hassan <155459839+muhammad-hassn@users.noreply.github.com>
2026-02-13 21:00:24 +0500

    Update DATABASE_URL to use environment variable

--- config/settings.py
+++ config/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 import dj_database_url
-DATABASE_URL=postgresql://postgres:jnLtNXaKQQwoAXMHQijoQcBAFCFLucvo@caboose.proxy.rlwy.net:39335/railway
+
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL')
