728de7cb2c04b84c5ca4bc98988c3beaf4e49017
muhammad-hassn <muhammadhassanmh4080@gmail.com>
2026-02-14 14:50:42 +0500

    Update changes

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-change-me-for-production-nasim-and-sons'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['hassanprojects.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -70,24 +70,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nasim_db',
+        'USER': 'nasim_user',
+        'PASSWORD': 'shariq@@',
+        'HOST': '127.0.0.1',
+        'PORT': '4080',
+    }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'nasim_db',
-#         'USER': 'nasim_user',
-#         'PASSWORD': 'shariq@@',
-#         'HOST': '127.0.0.1',
-#         'PORT': '4080',
-#     }
-# }
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
